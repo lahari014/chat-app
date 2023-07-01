22501a47f4d303578509e75341efc72a360209ec
Kommineni Lahari <82301982+lahari014@users.noreply.github.com>
2023-07-01 19:52:01 +0530

    Update index.js

--- src/Components/Item/index.js
+++ src/Components/Item/index.js
@@ -1,29 +1,49 @@
+import { Component } from "react";
 import "./index.css";
 
-const Item = (props) => {
-  const { details } = props;
-  const { id, name, text, color, display } = details;
-  const backgroundColor = color;
-  return (
-    <>
-      <li>
-        <div className="text">
-          <p className={`p1 ${backgroundColor}`}>{name}</p>
-          <p className="p3">{display}</p>
-        </div>
-        <div className="para">
-          <p className="p2">{text}</p>
-          <button className="but">
-            <img
-              src="https://media.istockphoto.com/id/1136351242/vector/like-social-media-hand-line-icon-editable-stroke-pixel-perfect-for-mobile-and-web.jpg?s=612x612&w=0&k=20&c=OnKkq5JCHkLvZ1Ck_njtTQMCyLljXsXGNhGqVpwwVUA="
-              className="image1"
-              alt="like-image"
-            />
-          </button>
-        </div>
-      </li>
-    </>
-  );
-};
+class Item extends Component {
+  state = { count: 0, click: false };
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+    this.setState({ click: true });
+  };
+
+  render() {
+    const { details } = this.props;
+    const { id, name, text, color, display } = details;
+    const backgroundColor = color;
+    const { count, click } = this.state;
+    return (
+      <>
+        <li>
+          <div className="text">
+            <p className={`p1 ${backgroundColor}`}>{name}</p>
+            <p className="p3">{display}</p>
+          </div>
+          <div className="para">
+            <p className="p2">{text}</p>
+            <button className="but" onClick={this.increment}>
+              {!click ? (
+                <img
+                  src="https://media.istockphoto.com/id/1136351242/vector/like-social-media-hand-line-icon-editable-stroke-pixel-perfect-for-mobile-and-web.jpg?s=612x612&w=0&k=20&c=OnKkq5JCHkLvZ1Ck_njtTQMCyLljXsXGNhGqVpwwVUA="
+                  className="image1"
+                  alt="like"
+                />
+              ) : (
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4926/4926585.png"
+                  className="image1"
+                  alt="liked"
+                />
+              )}
+              {count}
+            </button>
+          </div>
+        </li>
+      </>
+    );
+  }
+}
 
 export default Item;
